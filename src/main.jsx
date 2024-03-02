88b5a71bdeabd7977fb3673e0dfc04d3e17ea475
dvidalv <dvidalv@gmail.com>
2024-03-02 14:49:05 -0600

    Remove useState import from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './vendor/normalize.css';
