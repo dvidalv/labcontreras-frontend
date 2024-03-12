138a7684329da4789776a91221add52013f7683e
dvidalv <dvidalv@gmail.com>
2024-03-12 13:40:51 -0500

    Remove unused code related to token verification

--- src/routes/App/App.jsx
+++ src/routes/App/App.jsx
@@ -5,38 +5,11 @@ import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 import { useAppContext } from '../../contexts/MyContext';
 import { Outlet } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { checkToken } from '../../utils/auth';
 
 function App() {
-	const { isMenuOpen, setIsMenuOpen, loggedIn, setLoggedIn, token, setToken } =
+	const { isMenuOpen, setIsMenuOpen, loggedIn, setLoggedIn } =
 		useAppContext();
-	const navigate = useNavigate();
 
-
-	useEffect(() => {
-		const verifyToken = async () => {
-			const token = localStorage.getItem('token');
-			if (token) {
-				try {
-					const response = await checkToken(token);
-					if (response.status === 'success') {
-						setLoggedIn(true);
-						setToken(token);
-					} else {
-						localStorage.removeItem('token');
-						navigate('/signin');
-					}
-				} catch (error) {
-					localStorage.removeItem('token');
-					navigate('/signin');
-				}
-			}
-		};
-
-		verifyToken();
-	}, [setLoggedIn, setToken, navigate]);
 
 	return (
 		<div className="page__content">
