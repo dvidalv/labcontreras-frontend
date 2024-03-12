eeee7cd493a74848b5855747a84bd739663ebc28
dvidalv <dvidalv@gmail.com>
2024-03-12 18:54:05 -0500

    Update Map API key

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -5,7 +5,7 @@ import './map.css';
 function Map() {
 	useEffect(() => {
 		const loader = new Loader({
-			apiKey: import.meta.env.VITE_MAP_KEY,
+			apiKey: import.meta.env.MAP_KEY,
 			version: 'weekly',
 			libraries: ['marker'], // Asegúrate de incluir la biblioteca 'marker' necesaria para AdvancedMarkerElement
 		});
