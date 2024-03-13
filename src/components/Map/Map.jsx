76a214371a596f89502d727e2674c8912cc67fca
dvidalv <dvidalv@gmail.com>
2024-03-12 21:19:43 -0500

    Refactor Map component to use environment variables for API key

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -3,8 +3,10 @@ import { Loader } from '@googlemaps/js-api-loader';
 import './map.css';
 import config from '../../../config.js';
 
-function Map() {
 
+
+function Map() {
+	// let apiKey = process.env.REACT_APP_MAP_KEY;
 	let apiKey = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_MAP_KEY : config.MAP_KEY;
 	useEffect(() => {
 		const loader = new Loader({
