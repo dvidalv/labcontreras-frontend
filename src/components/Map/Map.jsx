9dc234627bf58c34a8604f00951596b39c9071ea
dvidalv <dvidalv@gmail.com>
2024-03-13 12:04:51 -0500

    Update Map component and map.css

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,27 +1,30 @@
-import React, { useEffect, useState } from 'react';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import './map.css';
 
 function Map() {
 	const position = {
 		lat: 19.460610613258446,
-		lng: -70.68062915211819
+		lng: -70.68062915211819,
 	};
 
-	return <div className="map">
-		<MapContainer center={position} zoom={18} scrollWheelZoom={false} className="map">
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    <Marker position={position}>
-      <Popup>
-        Laboratorio de Patología Contreras Robledo
-      </Popup>
-    </Marker>
-  </MapContainer>
-	</div>;
+	return (
+		<div className="map">
+			<MapContainer
+				center={position}
+				zoom={18}
+				scrollWheelZoom={true}
+				className="map"
+			>
+				<TileLayer
+					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+				/>
+				<Marker position={position}>
+					<Popup>Laboratorio de Patología Contreras Robledo</Popup>
+				</Marker>
+			</MapContainer>
+		</div>
+	);
 }
 
 export default Map;
-19.460610613258446, -70.68062915211819
