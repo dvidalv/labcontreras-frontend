525725e3c8173a28aa91cc9470f027340d5f64b3
dvidalv <dvidalv@gmail.com>
2024-04-11 16:56:33 -0500

    Refactor contact form and add form validation

--- src/routes/Forms/Contact/Contact.jsx
+++ src/routes/Forms/Contact/Contact.jsx
@@ -1,4 +1,7 @@
 import { Form, useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { contact } from '../../../utils/api';
 import Tooltip from '../../../components/ToolTip/Tooltip';
 import { useAppContext } from '../../../contexts/MyContext';
@@ -7,35 +10,49 @@ import './contact.css';
 
 function Contact() {
 	const location = 'contact';
+	const schema = z.object({
+		email: z.string().email(),
+		subject: z.string().min(3).max(50),
+		message: z.string().min(10).max(500),
+	});
 
-	const resetform = () => {
-		document.getElementById('contact-form').reset();
-	};
+	const {
+		register,
+		handleSubmit,
+		setError,
+		formState: { errors, isSubmitting },
+	} = useForm({
+		defaultValues: {
+			email: '',
+			subject: '',
+			message: '',
+		},
+		resolver: zodResolver(schema),
+	});
+
 
 	const navigation = useNavigate();
 
 	const { showTooltip, setShowTooltip, message, setMessage, type, setType } =
 		useAppContext();
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		const formData = new FormData(e.currentTarget);
-		const email = formData.get('email');
-		const subject = formData.get('subject');
-		const message = formData.get('message');
-
-		contact(email, subject, message).then((res) => {
+		const onSubmit = async (data) => {
+		const { email, subject, message } = data;
+		try {
+			const res = await contact(email, subject, message);
 			if (res.status === 'success') {
 				setShowTooltip(true);
 				setType('success');
 				setMessage('Mensaje enviado');
-				resetform();
-			} else {
-				setShowTooltip(true);
-				setType('error');
-				setMessage('Ha ocurrido un error al enviar el mensaje');
 			}
-		});
+		} catch (error) {
+			setShowTooltip(true);
+			setType('error');
+			setMessage('Ha ocurrido un error al enviar el mensaje');
+			setError('root', {
+				message: error.message,
+			});
+		}
 	};
 
 	return (
@@ -44,48 +61,48 @@ function Contact() {
 			<div className="form-container">
 				<h1>Contáctanos</h1>
 				<p>Si tienes alguna duda o necesitas ayuda, no dudes en contactarnos</p>
-				<Form id="contact-form" className="form" onSubmit={handleSubmit}>
-					<label className="form__label" htmlFor="email">
-						Email
-					</label>
+				<Form
+					id="contact-form"
+					className="form"
+					onSubmit={handleSubmit(onSubmit)}
+				>
 					<input
-						type="email"
-						id="email"
-						name="email"
-						required={true}
+						{...register('email')}
 						className="form__input"
 						placeholder="Direccion de correo electronico"
 					/>
+					<p className="form__error">{errors.email && errors.email.message}</p>
 
-					<label className="form__label" htmlFor="subject">
-						Asunto
-					</label>
 					<input
-						type="text"
-						id="subject"
-						name="subject"
-						required={true}
+						{...register('subject')}
 						className="form__input"
 						placeholder="Asunto de tu mensaje"
 					/>
+					<p className="form__error">
+						{errors.subject && errors.subject.message}
+					</p>
 
-					<label className="form__label" htmlFor="message">
-						Mensaje
-					</label>
 					<textarea
-						id="message"
-						name="message"
-						required={true}
+						{...register('message')}
 						className="form__input"
 						placeholder="Escribe tu mensaje aqui..."
 					/>
+					<p className="form__error">
+						{errors.message && errors.message.message}
+					</p>
 
-					<button type="submit" className="form__button">
-						Enviar
+					<button disabled={isSubmitting} type="submit" className="form__button">
+						{isSubmitting ? 'Enviando...' : 'Enviar'}
 					</button>
+					{errors.root && <p className="form__error">{errors.root.message}</p>}
 				</Form>
 				{showTooltip && (
-					<Tooltip message={message} type={type} location={location} className='tooltip--visible' />
+					<Tooltip
+						message={message}
+						type={type}
+						location={location}
+						className="tooltip--visible"
+					/>
 				)}
 				{navigation.state === 'loading' && (
 					<div
