54bf7ef2b5452a3d4d974fbb96f5aa53a093e409
dvidalv <dvidalv@gmail.com>
2024-04-11 22:21:36 -0500

    Refactor Signin component with react-hook-form and zod validation

--- src/routes/Forms/Sigin/Signin.jsx
+++ src/routes/Forms/Sigin/Signin.jsx
@@ -1,11 +1,26 @@
 import { Form, Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 import './Signin.css';
 import Tooltip from '../../../components/ToolTip/Tooltip';
 import { useAppContext } from '../../../contexts/MyContext';
 import { authorize } from '../../../utils/auth';
 import { useLocation, useNavigate } from 'react-router-dom';
 
+const schema = z.object({
+	email: z.string().email(),
+	password: z.string().min(6).max(12),
+});
+
+
 function Signin() {
+	
+	const { register, handleSubmit, formState: { errors, isSubmitting, isValid } } = useForm({
+		resolver: zodResolver(schema),
+		mode: 'onChange',
+	});
+	
 	const locationState = useLocation();
 	const navigate = useNavigate();
 	const { from } = locationState.state || { from: { pathname: '/' } };
@@ -21,11 +36,8 @@ function Signin() {
 		setLocation,
 	} = useAppContext();
 
-	async function handleSubmit(e) {
-		e.preventDefault();
-		const formData = new FormData(e.currentTarget);
-		const email = formData.get('email');
-		const password = formData.get('password');
+	async function handleForm(data) {
+		const { email, password } = data;
 		try {
 			const response = await authorize(email, password);
 			if (!response.ok) {
@@ -52,30 +64,29 @@ function Signin() {
 	return (
 		<div className="form-container">
 			<h1 className="form-container__title">Ingresar</h1>
-			<Form className="form" onSubmit={handleSubmit}>
+			<Form className="form" onSubmit={handleSubmit(handleForm)}>
 				<label className="form__label" htmlFor="email">
 					Correo electrónico
 				</label>
 				<input
-					type="email"
+					{...register('email')}
 					id="email"
-					name="email"
-					required={true}
 					className="form__input"
 					placeholder="Ingresa tu correo electrónico"
 				/>
+				<p className="form__error">{errors.email?.message}</p>
 
 				<label className="form__label" htmlFor="password">
 					Clave
 				</label>
 				<input
-					required={true}
+					{...register('password')}
 					type="password"
 					id="password"
-					name="password"
 					className="form__input"
 					placeholder="Ingresa tu clave"
 				/>
+				<p className="form__error">{errors.password?.message}</p>
 				<div className="form__links">
 					{/* <Link
 						to="/forgot-password"
@@ -88,9 +99,10 @@ function Signin() {
 					</Link>
 				</div>
 
-				<button type="submit" className="form__button">
-					Ingresar
+				<button disabled={!isValid || isSubmitting} type="submit" className={`form__button ${!isValid || isSubmitting ? 'disabled' : ''}`}>
+					{isSubmitting ? 'Enviando...' : 'Ingresar'}
 				</button>
+				<p className="form__error">{errors.root && errors.root.message}</p>
 			</Form>
 			{showTooltip && (
 				<Tooltip
